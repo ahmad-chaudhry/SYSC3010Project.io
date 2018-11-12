ee14e6609e2ae4424d34a81f1b2dd784aa9765c5
samyibrahim <43756930+samyibrahim@users.noreply.github.com>
2018-11-12 12:29:36 -0500

    Add files via upload
    Updated UDPReceive/Send.

--- BusCenter.java
+++ BusCenter.java
@@ -4,37 +4,44 @@ import java.net.*;
 
 public class BusCenter implements UDPCommunication{
 	
+	private static int receivedTotalPassengers;
 	private int totalPassengers;
 	
-	public void UDPSend(Packet x) {
-		DatagramSocket socket = null ;
-		try
-	      {
-	         // Convert the arguments first, to ensure that they are valid
-	         InetAddress host = InetAddress.getByName( "10.0.0.12" ) ;
-	         int port = 132;
-	         socket = new DatagramSocket() ;
-	         
-	         while (true)
-	         {
-	        		 String message = totalPassengers(numberOfPassengersEntering, numberOfPassengersExiting);
-	        		 if (message.length()==0) break;
-	        		 byte [] data = message.getBytes();
-	        		 DatagramPacket packet = new DatagramPacket( data, data.length, host, port ) ;
-	        		 socket.send( packet );
-	         } 
-	      }
-		catch( Exception e )
-	      {
+	public void UDPSend(InetAddress address, int portSend) {
+		try {
+			int portS = null;
+			DatagramSocket socket = null ;
+			InetAddress ip = InetAddress.getLocalHost();
+			byte buffer[] = null;
+			while(true) {
+				buf = totalPassengers;
+				DatagramPacket packet = new DatagramPacket(buffer,buffer.length,address,port); 
+				if(buffer.length == 0)break;
+				packet.send(packet);
+			}
+		}
+		catch( Exception e ){
 	         System.out.println( e ) ;
 	      }
-	      finally
-	      {
-	         if( socket != null )
-	            socket.close() ;
+	      finally{
+	         if( socket != null )socket.close() ;
 	      }
 	}
-	public void UDPReceive(Packet x) {
-		
+	public void UDPReceive(int portReceive) {
+		DatagramSocket socketR = new DatagramSocket(portReceive);
+		try {
+			byte[] receive = new byte[65535];
+			DatagramPacket packetR = null;
+			while(true) {
+				packetR = new DatagramPacket(receive,receive.length);
+				if(receive.length == 0)break;
+				receivedTotalPassengers = socketR.receive(packetR);
+				receive = new byte[65535];
+			}
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
 	}
+
 }
