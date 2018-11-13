b282d996b4569159e13bed780631104d40b86bd3
samyibrahim <43756930+samyibrahim@users.noreply.github.com>
2018-11-13 12:31:01 -0500

    Add files via upload
    added new method to update database in bus center

--- BusCenter.java
+++ BusCenter.java
@@ -1,22 +1,28 @@
-package 3010Project;
+package syscThirdYear;
 
 import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.ByteBuffer;
 
 public class BusCenter implements UDPCommunication{
 	
+	//Pretend database until partner finished Database
+	private BusDatabase db = new BusDatabase();
+	//global variable to keep track of the total passengers
 	private static int receivedTotalPassengers;
 	private int totalPassengers;
 	
+	
+	//Send value from bus center
 	public void UDPSend(InetAddress address, int portSend) {
+		DatagramSocket socket = null ;
 		try {
-			int portS = null;
-			DatagramSocket socket = null ;
+			int portS;
 			InetAddress ip = InetAddress.getLocalHost();
 			byte buffer[] = null;
-				buf = totalPassengers;
-				DatagramPacket packet = new DatagramPacket(buffer,buffer.length,address,portSend); 
-				if(buffer.length == 0)break;
-				packet.send(packet);
+			buffer = ByteBuffer.allocate(4).putInt(totalPassengers).array();
+			DatagramPacket packet = new DatagramPacket(buffer,buffer.length,address,portSend); 
+			socket.send(packet);
 		}
 		catch( Exception e ){
 	         System.out.println( e ) ;
@@ -25,21 +31,30 @@ public class BusCenter implements UDPCommunication{
 	         if( socket != null )socket.close() ;
 	      }
 	}
+	
+	//Receive values (of traffic on Bus) from the bus and store it 
 	public void UDPReceive(int portReceive) {
-		DatagramSocket socketR = new DatagramSocket(portReceive);
+		DatagramSocket socketR;
 		try {
 			byte[] receive = new byte[65535];
+			socketR = new DatagramSocket(portReceive);
 			DatagramPacket packetR = null;
-				packetR = new DatagramPacket(receive,receive.length);
-				if(receive.length == 0)break;
-				receivedTotalPassengers = socketR.receive(packetR);
-				receive = new byte[65535];
+			packetR = new DatagramPacket(receive,receive.length);
+			socketR.receive(packetR);
+			receive = new byte[65535];
+			//return 
 		}
 		catch(Exception e) {
 			System.out.println(e);
 		}
 	}
-	public static void main(String[] args) {
+	
+	//Add value to the Database that has been received from the BUS
+	public void updateDatabase(int x) {
+		db.addToDB(x);
+	}
+	
+	public void main(String[] args) {
 		UDPReceive(1678);
 	}
 
