2c63f5e736e663d598b7b3b37ca92751036280a6
samyibrahim <43756930+samyibrahim@users.noreply.github.com>
2018-11-20 14:44:04 -0500

    Add files via upload
    Updated Bus Center

--- BusCenter.java
+++ BusCenter.java
@@ -1,34 +1,39 @@
 package syscThirdYear;
 
-import java.io.*;
 import java.util.*;
 import java.net.*;
-import java.nio.*;
 
 public class BusCenter{
 	
-	//Pretend database until partner finished Database
-	private ArrayList<Integer> dataBase = new ArrayList<Integer>();
+	//Final string received
+	private static String receivedStr;
+	List<String> dbUpdate;
 	
-	//global variable to keep track of the total passengers
-	private int totalPassengers;
-	
-	//get pretend database
-	public ArrayList<Integer> getDB() {
-		return dataBase;
-	}
+	//Variables to send to the Database
+	private String finalBusID;
+	private String finalBusZone;
 	
 	//Array List to keep track of all busses
 	private static ArrayList<Bus> allBusses = new ArrayList<Bus>();
 	
+	//Pretend database until partner finished Database
+	private ArrayList<String> dataBase = new ArrayList<String>();
+		
 	//getter for ArrayList
-	public ArrayList<Bus> getList(){
-		return allBusses;
-	}
-	//Add new bus to our list of busses
-	public void addBusToList(Bus x) {
+	public void addBusToList(Bus x){
 		allBusses.add(x);
 	}
+	//get list of busses
+	public ArrayList<Bus> getBusList() {
+		return allBusses;
+	}
+	
+
+	//get pretend database
+	public ArrayList<String> getDB() {
+		return dataBase;
+	}
+	
 	//Receive values (of traffic on Bus) from the bus and store it 
 	public void UDPReceive(int portReceive) {
 		try {
@@ -37,20 +42,48 @@ public class BusCenter{
 			DatagramPacket packetR = new DatagramPacket(buffer,buffer.length);
 			socketR.receive(packetR);
 			System.out.println(new String(packetR.getData()).trim() ) ;
-			//InetAddress addressSendBack = packetR.getAddress();
-			//int portSendBack = packetR.getPort();
-			//packetR = new DatagramPacket(buffer, buffer.length,addressSendBack,portSendBack);
-			//socketR.send(packetR);
-			
+			receivedStr = new String(packetR.getData());
+			dbUpdate = Arrays.asList(receivedStr.split(","));
 		}
 		catch(Exception e) {
 			System.out.println(e);
 		}
 	}
 	
+	//returns string being received through UDP receive
+	public String getReceivedString() {
+		return receivedStr;
+	}
+	
+	//Sends an ACK message back to the 
+	@SuppressWarnings("resource")
+	public void testUDPReceive(int portReceive) {
+		try {
+			DatagramSocket socketR = new DatagramSocket(portReceive);
+			byte[] buffer = new byte[256];
+			DatagramPacket packetR = new DatagramPacket(buffer,buffer.length);
+			socketR.receive(packetR);
+			System.out.println(new String(packetR.getData()).trim() ) ;
+			InetAddress addressSendBack = packetR.getAddress();
+			int portSendBack = packetR.getPort();
+			packetR = new DatagramPacket(buffer, buffer.length,addressSendBack,portSendBack);
+			socketR.send(packetR);
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+	//Update global variables used to update the database
+	public void updateReceivedVariables() {
+		finalBusID = dbUpdate.get(0);
+		finalBusZone = dbUpdate.get(1);
+	}
+	
 	//Add value to the Database that has been received from the BUS
-	public void updateDatabase(int x) {
-		dataBase.add(x);
+	public void updateDatabase() {
+		dataBase.add(finalBusID);
+		dataBase.add(finalBusZone);
 	}
 	
 	public static void main(String[] args) {
