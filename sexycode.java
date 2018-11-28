14ecc41b00ea5062f8fb47c0143ffde9d9dad24b
anannyabhatia <43757391+anannyabhatia@users.noreply.github.com>
2018-11-28 10:32:53 -0500

    Update and rename updatedata.java to sexycode.java

--- sexycode.java
+++ sexycode.java
@@ -7,7 +7,6 @@ import java.util.Scanner;
 public class updatedata {
 	
 	static Connection conn = null;
-	static Scanner scan = new Scanner(System.in);
 	static ArrayList<String> columns;
 	static ArrayList<String> colvalues;
 
@@ -27,8 +26,6 @@ public class updatedata {
 				conn = DriverManager.getConnection(url,userName,password); 
 				
 
-				System.out.println("Enter the bus number you want to refresh");
-				String busnumber = scan.nextLine();
 				columns = new ArrayList<String>();
 				
 				DatabaseMetaData md = conn.getMetaData();
@@ -39,23 +36,10 @@ public class updatedata {
 				}
 				
 				
-				
-				System.out.println("Enter the new details");
-				colvalues = new ArrayList<String>();
-				for (int i = 0; i< columns.size();i++)
-				{
-					System.out.println(columns.get(i) + ": ");
-					String change = scan.next();
-					colvalues.add(change);
-				}
-				
-				
-				
 				Statement stmt = conn.createStatement();
 				for (int i = 0; i<columns.size(); i++)
 				{
-					String query = "UPDATE details3 SET " + columns.get(i) + " = '" + colvalues.get(i)      
-					+ "' WHERE " + "  busnumber = " + busnumber;
+					String query = "UPDATE details3 SET capacityzones = '2' where busnumber = 4"; 
 					stmt.executeUpdate(query);
 				}
 				
