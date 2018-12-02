78531c24d94f38916703eb7f58cd229f5b6879a2
Jaskarans101 <32146616+Jaskarans101@users.noreply.github.com>
2018-12-02 15:47:55 -0500

    Add files via upload
    Version 2.2 of the application Currently can show data  all 5 bus routes that come to Carleton U(4,10,104,111 and OTrain).

--- Hero.java
+++ Hero.java
@@ -1,12 +1,37 @@
 package com.example.jaska.buspassengertraffic;
 
 public class Hero {
-    private String Bus1;
-    private String Bus2;
-    public Hero(String bus1, String bus2) {
-        this.Bus1 = bus1;
-        this.Bus2 = bus2; }
-    public String getBus1() {
-        return Bus1; }
-    public String getBus2() {
-        return Bus2; }}
+    private String capacityzone1;
+    private String capacityzone2;
+    private String capacityzone3;
+    private String capacityzone4;
+    private String capacityzone5;
+
+    public Hero(String capacityzone1, String capacityzone2, String capacityzone3, String capacityzone4, String capacityzone5) {
+        this.capacityzone1 = capacityzone1;
+        this.capacityzone2 = capacityzone2;
+        this.capacityzone3 = capacityzone3;
+        this.capacityzone4 = capacityzone4;
+        this.capacityzone5 = capacityzone5;
+    }
+
+    public String getCapacityzone1() {
+        return capacityzone1;
+    }
+
+    public String getCapacityzone2() {
+        return capacityzone2;
+    }
+
+    public String getCapacityzone3() {
+        return capacityzone3;
+    }
+
+    public String getCapacityzone4() {
+        return capacityzone4;
+    }
+
+    public String getCapacityzone5() {
+        return capacityzone5;
+    }
+}
--- MainActivity.java
+++ MainActivity.java
@@ -1,4 +1,5 @@
 package com.example.jaska.buspassengertraffic;
+
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -6,7 +7,9 @@ import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -16,66 +19,79 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
     private TextView busNum;
     private TextView showData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         busNum = findViewById(R.id.busNum);
-        showData = findViewById(R.id.showData);}
+        showData = findViewById(R.id.showData);
+    }
 
     public void getData(View v) {
         Retrofit retrofit = new Retrofit.Builder().baseUrl(Api.BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
         Api api = retrofit.create(Api.class);
         Call<List<Hero>> call = api.getBus();
+        //Call<List<time>> call = api.gettime();
         call.enqueue(new Callback<List<Hero>>() {
             @Override
             public void onResponse(Call<List<Hero>> call, Response<List<Hero>> response) {
                 ProgressBar prg;
-                prg = (ProgressBar)findViewById(R.id.progressBar);
+                prg = (ProgressBar) findViewById(R.id.progressBar);
                 prg.setMinimumHeight(80);
-                  prg.setMinimumWidth(100);
+                prg.setMinimumWidth(100);
                 List<Hero> busses = response.body();
                 if (busNum.getText().toString().equals("")) {
                     Toast.makeText(getApplicationContext(), "Enter a valid Bus number", Toast.LENGTH_SHORT).show();
-                    return;}
-                int num =Integer.parseInt(busNum.getText().toString());
-                if ( num== 1) {
+                    return;
+                }
+                int num = Integer.parseInt(busNum.getText().toString());
+
+                if (num == 2) {
                     Hero bus = busses.get(0);
-                        if (bus.getBus1().equals("Green")){
-                            showData.setText("Empty");
-                            showData.setTextColor(Color.GREEN);
-                            prg.setProgress(25);
-                            prg.getProgressDrawable().setColorFilter(Color.GREEN, android.graphics.PorterDuff.Mode.SRC_IN);
-                            } else if (bus.getBus1().equals("Yellow")){
-                            showData.setText("Half Full");
-                            showData.setTextColor(Color.YELLOW);
-                            prg.setProgress(50);
-                            prg.getProgressDrawable().setColorFilter(Color.YELLOW, android.graphics.PorterDuff.Mode.SRC_IN);
-                            } else if (bus.getBus1().equals("Red")){
-                            showData.setText("Almost Full");
-                            showData.setTextColor(Color.RED);
-                            prg.setProgress(80);
-                            prg.getProgressDrawable().setColorFilter(Color.RED, android.graphics.PorterDuff.Mode.SRC_IN); }
-                            } else if (num == 2) {
-                             Hero bus = busses.get(1);
-                             if (bus.getBus2().equals("Green")){
-                               showData.setText("Empty");
-                               showData.setTextColor(Color.GREEN);
-                               prg.setProgress(25);
-                              prg.getProgressDrawable().setColorFilter(Color.GREEN, android.graphics.PorterDuff.Mode.SRC_IN);
-                              } else if (bus.getBus2().equals("Yellow")){
-                                 showData.setText("Half Full");
-                                 showData.setTextColor(Color.YELLOW);
-                                 prg.setProgress(50);
-                                 prg.getProgressDrawable().setColorFilter(Color.YELLOW, android.graphics.PorterDuff.Mode.SRC_IN);
-                             } else if (bus.getBus2().equals("Red")){
-                                showData.setText("Almost Full");
-                                showData.setTextColor(Color.RED);
-                                 prg.setProgress(80);
-                                 prg.getProgressDrawable().setColorFilter(Color.RED, android.graphics.PorterDuff.Mode.SRC_IN); }
-                                 }else{
-                                   Toast.makeText(getApplicationContext(), "Enter a valid Bus number", Toast.LENGTH_SHORT).show(); }
-                                   }
+                    if (bus.getCapacityzone1().equals("green")) {
+                        showData.setText("Empty");
+                        showData.setTextColor(Color.GREEN);
+                        prg.setProgress(25);
+                        prg.getProgressDrawable().setColorFilter(Color.GREEN, android.graphics.PorterDuff.Mode.SRC_IN);
+                    } else if (bus.getCapacityzone1().equals("yellow")) {
+                        showData.setText("Half Full");
+                        showData.setTextColor(Color.YELLOW);
+                        prg.setProgress(50);
+                        prg.getProgressDrawable().setColorFilter(Color.YELLOW, android.graphics.PorterDuff.Mode.SRC_IN);
+                    } else if (bus.getCapacityzone1().equals("red")) {
+                        showData.setText("Almost Full");
+                        showData.setTextColor(Color.RED);
+                        prg.setProgress(80);
+                        prg.getProgressDrawable().setColorFilter(Color.RED, android.graphics.PorterDuff.Mode.SRC_IN);
+                    } else  {
+                        showData.setText("ERROR");
+                        showData.setTextColor(Color.RED);
+
+                    }
+                } else if (num == 7) {
+                    Hero bus = busses.get(1);
+                    if (bus.getCapacityzone2().equals("green")) {
+                        showData.setText("Empty");
+                        showData.setTextColor(Color.GREEN);
+                        prg.setProgress(25);
+                        prg.getProgressDrawable().setColorFilter(Color.GREEN, android.graphics.PorterDuff.Mode.SRC_IN);
+                    } else if (bus.getCapacityzone2().equals("yellow")) {
+                        showData.setText("Half Full");
+                        showData.setTextColor(Color.YELLOW);
+                        prg.setProgress(50);
+                        prg.getProgressDrawable().setColorFilter(Color.YELLOW, android.graphics.PorterDuff.Mode.SRC_IN);
+                    } else if (bus.getCapacityzone2().equals("red")) {
+                        showData.setText("Almost Full");
+                        showData.setTextColor(Color.RED);
+                        prg.setProgress(80);
+                        prg.getProgressDrawable().setColorFilter(Color.RED, android.graphics.PorterDuff.Mode.SRC_IN);
+                    }
+
+                } else {
+                    Toast.makeText(getApplicationContext(), "Enter a valid Bus number", Toast.LENGTH_SHORT).show();
+                }
+            }
 
 
             @Override
