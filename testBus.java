738fe2d92a825c520b2e4b8e9f4da20b31f11027
samyibrahim <43756930+samyibrahim@users.noreply.github.com>
2018-12-08 12:54:35 -0500

    Add files via upload
    Final Bus Test

--- testBus.java
+++ testBus.java
@@ -1,13 +1,11 @@
 package projectTests;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.awt.event.ActionListener;
 import java.net.InetAddress;
 import java.util.Timer;
-
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-import junit.framework.Assert;
 import junit.framework.TestCase;
 import syscThirdYear.Bus;
 import syscThirdYear.BusCenter;
@@ -18,6 +16,7 @@ class testBus extends TestCase{
 	private InetAddress address;
 	private int port;
 	
+	/**
 	@Test 
 	void testUDPSend(){
 		Bus testBus = new Bus();
@@ -25,21 +24,20 @@ class testBus extends TestCase{
 		testBus.UDPSend(testBus, address, port);
 		//this should send the total number of passengers to a chosen IP address on a certain port
 	}
+	**/
 	
-	@Test
-	void testUDPReceive() {
-		Bus testBus = new Bus();
-		testBus.UDPSend(testBus, address, port);
-		testBC.UDPReceive(port);
-	}
 	
 	@Test
 	void testBusCapacityZone() {
-		Bus testBus = new Bus();
+		Bus testBus = new Bus(99);
 		testBus.addPassenger();
+		testBus.addPassenger();
+		testBus.updateBusCapacityZone();
 		String testZone = testBus.getBusCapacityZone();
 		Assert.assertEquals(testZone, "green");
 	}
+	
+	/**
 	@Test
 	void testUDPSpeed() {
 		int testCount = 0;
@@ -51,25 +49,27 @@ class testBus extends TestCase{
 		long secondsDisplay = elapsedSeconds % 60;
 		long elapsedMinutes = elapsedSeconds / 60;
 	}
+	**/
 	@Test
-	void testAddPassenger(int x) {
-		Bus testBus = new Bus();
-		for(int i=0;i<x;i++) {
-			testBus.addPassenger();
-		}
+	void testAddPassenger() {
+		Bus testBus = new Bus(99);
+		testBus.addPassenger();
+		testBus.addPassenger();
+		testBus.addPassenger();
+		testBus.addPassenger();
 		int finalans = testBus.totalPassengers();
-		Assert.assertEquals(finalans, x);  
+		Assert.assertEquals(finalans, 4);  
 	}
 	@Test
-	void testRemovePassenger(int totalPassengers,int remove) {
-		Bus testBus = new Bus();
-		for(int i=0;i<totalPassengers;i++) {
-			testBus.addPassenger();
-		}
-		for(int i=0;i<remove;i++) {
-			testBus.addPassenger();
-		}
+	void testRemovePassenger() {
+		Bus testBus = new Bus(99);
+		testBus.addPassenger();
+		testBus.addPassenger();
+		testBus.addPassenger();
+		testBus.addPassenger();
+		testBus.removePassenger();
+		testBus.removePassenger();
 		int finalans = testBus.totalPassengers();
-		Assert.assertEquals(finalans, totalPassengers-remove);
+		Assert.assertEquals(finalans, 2);  
 	}
 }
