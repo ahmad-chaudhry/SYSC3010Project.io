8e828a5a27e0b718939f308e0aa4841e25f74bdc
samyibrahim <43756930+samyibrahim@users.noreply.github.com>
2018-12-08 12:54:52 -0500

    Add files via upload
    Final Bus Center Tests

--- testBusCenter.java
+++ testBusCenter.java
@@ -1,39 +1,44 @@
 package projectTests;
 
-import static org.junit.jupiter.api.Assertions.*;
 import java.net.InetAddress;
+
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-import junit.framework.Assert;
 import junit.framework.TestCase;
 import syscThirdYear.Bus;
 import syscThirdYear.BusCenter;
 
 class testBusCenter extends TestCase{
 	
-	private static int passengersEntered = 5;
-	private static int passengersExited = 4;
-	BusCenter testBC = new BusCenter();
-	String testBCZone = "green";
 	private InetAddress address;
 	private int port;
-	private int finalAns = passengersEntered - passengersExited;
-	
-	@Test 
-	void testUDPSend(){
-		Bus testBus = new Bus();
-		testBus.UDPSend(testBus, address, port);
-		//this should send the total number of passengers to a chosen IP address on a certain port
-	}
-	
+	/**
 	@Test
 	void testUDPReceive() {
+		BusCenter testBC = new BusCenter();
 		Bus testBus = new Bus();
+		testBus.addPassenger();
+		testBus.addPassenger();
 		testBus.UDPSend(testBus, address, port);
 		testBC.UDPReceive(port);
+		Assert.assertEquals(testBC.getReceivedString(), "2");
 	}
+	**/
+	
 	@Test
-	void testUpdateDatabase(int x) {
-		testBC.updateDatabase(x);
+	void testAddBusToList(Bus x) {
+		BusCenter testBC = new BusCenter();
+		testBC.addBusToList(x);
+		Assert.assertEquals(testBC.getBusList().contains(x), true);
+	}
+	/**
+	 * @Test
+		void testUpdateDatabase(Integer x) {
+		BusCenter testBC = new BusCenter();
+		//testBC.updateDatabase(x);
 		Assert.assertEquals(testBC.getDB().contains(x), true);
 	}
+	 * @param x
+	 */
+	
 }
